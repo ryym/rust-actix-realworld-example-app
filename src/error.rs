aab33654fef9a734cb9a630a70820e3eb98c7fc7
ryym <ryym.64@gmail.com>
2018-10-21 13:10:25 +0900

    feat: Implement basic error type with error kinds

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,64 @@
-// TODO: Define my error types.
+// https://github.com/rust-lang-nursery/failure
+// https://rust-lang-nursery.github.io/failure/error-errorkind.html
+
+use failure::{Backtrace, Context, Fail};
+use std::fmt::{self, Display};
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+#[derive(Clone, Debug, Fail)]
+pub enum ErrorKind {
+    #[fail(display = "{}", _0)]
+    Misc(String),
+    // Auth, Validation, etc
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+}
+
+impl<S> From<Context<S>> for Error
+where
+    S: Into<String> + Display + Sync + Send,
+{
+    fn from(ctx: Context<S>) -> Error {
+        Error {
+            inner: ctx.map(|s| ErrorKind::Misc(s.into())),
+        }
+    }
+}
