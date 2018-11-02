7b45b5b2572299a2aa6f70d02a69afa5d9f0ac88
ryym <ryym.64@gmail.com>
2018-11-02 22:44:30 +0900

    test(tags): Add test for /api/tags

--- src/app/tags/mod.rs
+++ src/app/tags/mod.rs
@@ -14,3 +14,34 @@ where
     let tags = hub.use_db(|conn| hub.list_tags(conn))?;
     Ok(Json(TagListResponse { tags }))
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use actix_web::{test::TestRequest, FromRequest, State};
+    use crate::{db, test};
+
+    #[test]
+    fn list_returns_tag_response() -> Result<()> {
+        struct Mock {
+            conn: db::Conn,
+        }
+        impl_have_db!(Mock(conn));
+
+        impl CanListTags for Mock {
+            fn list_tags(&self, _: &db::Conn) -> Result<Vec<String>> {
+                Ok(vec!["tag_a".to_owned(), "tag_b".to_owned()])
+            }
+        }
+
+        let t = test::init()?;
+
+        let mock = Mock { conn: t.db_conn()? };
+        let req = TestRequest::with_state(mock).finish();
+        let state = State::extract(&req);
+        let res = list(state)?;
+        assert_eq!(res.tags, &["tag_a", "tag_b"]);
+
+        Ok(())
+    }
+}
